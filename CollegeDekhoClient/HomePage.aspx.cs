dff0da1d1f2690935f05d65dfa3dc293e21301da
Om Dhingani <omdhingani0@gmail.com>
2024-04-02 23:20:03 +0530

    search functionality

--- CollegeDekhoClient/HomePage.aspx.cs
+++ CollegeDekhoClient/HomePage.aspx.cs
@@ -12,15 +12,24 @@ namespace CollegeDekhoClient
 {
     public partial class HomePage : System.Web.UI.Page
     {
+        
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindColleges();
+            }
+        }
+
+        protected void BindColleges()
         {
             CollegeReference.CollegeServiceClient cs = new CollegeReference.CollegeServiceClient();
             List<College> collegeList = cs.listOfAllCollege().ToList();
-            collegeRepeater.DataSource = collegeList; 
+            collegeRepeater.DataSource = collegeList;
             collegeRepeater.DataBind();
         }
 
-        
+
 
         protected void collegeCardPanel_Click(object sender, EventArgs e)
         {
@@ -34,5 +43,30 @@ namespace CollegeDekhoClient
         {
             Response.Redirect("AdminLogin.aspx");
         }
+
+
+        protected void searchTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string query = searchTextBox.Text.Trim();
+            SearchData(query);
+        }
+
+        protected void SearchData(string query)
+        {
+            // Fetch data from the WCF service
+            CollegeReference.CollegeServiceClient cs = new CollegeReference.CollegeServiceClient();
+            List<College> collegeList = cs.listOfAllCollege().ToList();
+            List<College> filteredData = collegeList.Where(item =>
+                item.Name.Contains(query) ||
+                item.Place.Contains(query)
+            ).ToList();
+
+            // Bind the filtered data to your UI element
+            collegeRepeater.DataSource = filteredData;
+            collegeRepeater.DataBind();
+        }
+
+
+
     }
 }
--- CollegeDekhoClient/HomePage.aspx.designer.cs
+++ CollegeDekhoClient/HomePage.aspx.designer.cs
@@ -24,6 +24,15 @@ namespace CollegeDekhoClient
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
+        /// searchTextBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox searchTextBox;
+
+        /// <summary>
         /// collegeRepeater control.
         /// </summary>
         /// <remarks>
